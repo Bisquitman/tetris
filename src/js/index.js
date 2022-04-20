d4441377fad2709282112ad42da1a50a2b42b2c9
Bisquitman <lynx8309@gmail.com>
2022-04-20 12:42:17 +0300

    Day 2

--- src/js/index.js
+++ src/js/index.js
@@ -32,22 +32,63 @@ const game = {
       ['o', 'x', 'o'],
       ['o', 'x', 'o'],
       ['x', 'x', 'o'],
-    ]
+    ],
+    rotationIndex: 0,
+    rotation: [
+      [
+        ['o', 'x', 'o'],
+        ['o', 'x', 'o'],
+        ['x', 'x', 'o'],
+      ],
+      [
+        ['x', 'o', 'o'],
+        ['x', 'x', 'x'],
+        ['o', 'o', 'o'],
+      ],
+      [
+        ['o', 'x', 'x'],
+        ['o', 'x', 'o'],
+        ['o', 'x', 'o'],
+      ],
+      [
+        ['o', 'o', 'o'],
+        ['x', 'x', 'x'],
+        ['o', 'o', 'x'],
+      ],
+    ],
   },
 
   moveLeft() {
-    this.activeTetramino.x -= 1;
+    if (this.checkOutPosition(this.activeTetramino.x - 1, this.activeTetramino.y)) {
+      this.activeTetramino.x -= 1;
+    }
   },
 
   moveRight() {
-    this.activeTetramino.x += 1;
+    if (this.checkOutPosition(this.activeTetramino.x + 1, this.activeTetramino.y)) {
+      this.activeTetramino.x += 1;
+    }
   },
 
   moveDown() {
-    this.activeTetramino.y += 1;
+    if (this.checkOutPosition(this.activeTetramino.x, this.activeTetramino.y + 1)) {
+      this.activeTetramino.y += 1;
+    } else {
+      this.stopMove();
+    }
   },
 
-  rotateTetramino() { },
+  rotateTetramino() {
+    this.activeTetramino.rotationIndex = this.activeTetramino.rotationIndex < 3 ? this.activeTetramino.rotationIndex + 1 : 0;
+
+    this.activeTetramino.block = this.activeTetramino.rotation[this.activeTetramino.rotationIndex];
+
+    if (!this.checkOutPosition(this.activeTetramino.x, this.activeTetramino.y)) {
+      this.activeTetramino.rotationIndex = this.activeTetramino.rotationIndex > 0 ? this.activeTetramino.rotationIndex - 1 : 3;
+
+      this.activeTetramino.block = this.activeTetramino.rotation[this.activeTetramino.rotationIndex];
+    };
+  },
 
   get viewArea() {
     const area = JSON.parse(JSON.stringify(this.area));
@@ -56,12 +97,41 @@ const game = {
     for (let i = 0; i < tetramino.length; i++) {
       const row = tetramino[i];
       for (let j = 0; j < row.length; j++) {
-        if (row[j] === 'x') {
+        if (row[j] !== 'o') {
           area[y + i][x + j] = tetramino[i][j];
         }
       }
     }
     return area;
+  },
+
+  checkOutPosition(x, y) {
+    const tetramino = this.activeTetramino.block;
+    for (let i = 0; i < tetramino.length; i++) {
+      for (let j = 0; j < tetramino[i].length; j++) {
+        if (tetramino[i][j] === 'o') continue;
+
+        if (!this.area[y + i] ||
+          !this.area[y + i][x + j] ||
+          this.area[y + i][x + j] === 'x') {
+          return false;
+        }
+      }
+    }
+    return true;
+  },
+
+  stopMove() {
+    const { x, y, block: tetramino } = this.activeTetramino;
+
+    for (let i = 0; i < tetramino.length; i++) {
+      const row = tetramino[i];
+      for (let j = 0; j < row.length; j++) {
+        if (row[j] !== 'o') {
+          this.area[y + i][x + j] = tetramino[i][j];
+        }
+      }
+    }
   },
 };
 
@@ -77,12 +147,14 @@ canvas.height = canvas.width * 2;
 const context = canvas.getContext('2d');
 
 const showArea = (area) => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
   for (let y = 0; y < area.length; y++) {
     const line = area[y];
 
     for (let x = 0; x < line.length; x++) {
       const block = line[x];
-      if (block === 'x') {
+      if (block !== 'o') {
         context.fillStyle = '#3f484d';
         context.strokeStyle = 'white';
         context.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
@@ -91,5 +163,35 @@ const showArea = (area) => {
     }
   }
 };
+
+window.addEventListener('keydown', (event) => {
+  const key = event.code;
+
+  switch (key) {
+    case 'ArrowLeft':
+    case 'KeyA':
+      game.moveLeft();
+      showArea(game.viewArea);
+      break;
+    case 'ArrowRight':
+    case 'KeyD':
+      game.moveRight();
+      showArea(game.viewArea);
+      break;
+    case 'ArrowDown':
+    case 'KeyS':
+      game.moveDown();
+      showArea(game.viewArea);
+      break;
+    case 'ArrowUp':
+    case 'KeyW':
+      game.rotateTetramino();
+      showArea(game.viewArea);
+      break;
+
+    default:
+      break;
+  }
+});
 
 showArea(game.viewArea);
